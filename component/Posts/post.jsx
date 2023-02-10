bf0d334f54aef34c617ba68e8483920e978ee8b4
Diseive <104061331+Diseive@users.noreply.github.com>
2023-02-10 16:29:21 +0900

    commit

--- component/Posts/post.jsx
+++ component/Posts/post.jsx
@@ -12,8 +12,10 @@ export default function Posts({ posts }) {
               <Image
                 src={eyecatch.url}
                 alt=""
-                layout="fill"
+                layout="responsive"
                 objectFit="cover"
+                width={eyecatch.width}
+                height={eyecatch.height}
                 sizes="(min-width: 1152px) 576px, 50vw"
                 placeholder="blur"
                 blurDataURL={eyecatch.blurDataURL}
